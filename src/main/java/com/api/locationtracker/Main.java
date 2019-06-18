357ce2934bd0e3f9d12587e987f926d8c9932354
Jeremy <gencavjj@gmail.com>
2019-06-18 19:55:11 -0400

    added a SteamLambdaHandler class

--- src/main/java/com/api/locationtracker/Main.java
+++ src/main/java/com/api/locationtracker/Main.java
@@ -1,15 +1,13 @@
 package com.api.locationtracker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
-
 }
